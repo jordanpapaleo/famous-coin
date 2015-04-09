2a74209e76a518b25be4fcfc1b276fcd6bd45bda
Jordan Papaleo <papaleowebdev@gmail.com>
2015-04-09 13:00:11 -0700

    Fixed what I broke in timeline

--- src/shared/Timeline.js
+++ src/shared/Timeline.js
@@ -4,7 +4,6 @@ const Famous         = core.Famous;
 const Size           = components.Size;
 const Easing         = transitions.Easing;
 const Transitionable = transitions.Transitionable;
-
 var easingCurveLinear = Easing.getCurve('linear');
 
 export class Timeline {
@@ -13,7 +12,8 @@ export class Timeline {
         this.timescale = options.timescale || 1;
         this.currentTime = new Transitionable(0);
         this.callbacks = [];
-        this.direction = null;
+        this.direction;
+
         //for now update on all ticks until clock gets fixed
         Famous.getClock().update(this);
     }
@@ -21,6 +21,11 @@ export class Timeline {
     registerComponent(animationData) {
         this.componentSet.push(animationData);
     }
+
+    /**
+     * @param callbackData object
+     * { time: 1000, direction: 1 || 0, fn: function() { } }
+     */
 
     registerCallback(callbackData) {
         callbackData.direction = callbackData.direction || 1; //what direction to trigger this callback.
@@ -35,28 +40,27 @@ export class Timeline {
     /*eslint no-underscore-dangle:0*/
     addKeyframes(keyframeObjs) {
         var layers = [];
-        var keyframe, nodeProperty, id, i, j;
-
+        var keyframe;
+        var nodeProperty;
+        var id;
+        var i;
+        var j;
         for (i = 0; i < keyframeObjs.length; i++) {
             var keyframes = keyframeObjs[i].keyframes;
-
             for(j = 0; j < keyframes.length; j++) {
                 keyframe = keyframes[j];
                 nodeProperty = keyframe.shift();
                 id = nodeProperty._dispatch._renderProxy._id;
                 keyframe.unshift(keyframeObjs[i].time);
-
                 if (!layers[id]) {
                     layers[id] = {
                         component: nodeProperty,
                         path: []
                     };
                 }
-
                 layers[id].path.push(keyframe);
             }
         }
-
         for (id in layers) {
             this.registerComponent(layers[id]);
         }
@@ -72,7 +76,6 @@ export class Timeline {
                 // Famous.getClock().noLongerUpdate(this);
                 this.update();
                 this.inTransition = false;
-
                 if (callback) {
                     callback();
                 }
@@ -88,31 +91,30 @@ export class Timeline {
     update(time) {
         if (this.inTransition) {
             var res = [];
-            const _this = this;
             time = this.currentTime.get() * this.timescale;
 
-            this.callbacks.forEach(function(callback) {
-                if(_this.direction > 0 && callback.direction > 0) {
+            for(var i=0; i<this.callbacks.length; i++) {
+                if(this.direction > 0 && this.callbacks[i].direction > 0) {
                     //forward
-                    if(time >= callback.time) {
-                        callback.fn();
-                        callback.direction = -1; //set to backwards
+                    if(time >= this.callbacks[i].time) {
+                        this.callbacks[i].fn();
+                        this.callbacks[i].direction = -1; //set to backwards
                     }
-                } else if(_this.direction < 0 && callback.direction < 0){
-                    if(time <= callback.time) {
-                        callback.fn();
-                        callback.direction = 1; //set to forwards
+                } else if(this.direction < 0 && this.callbacks[i].direction < 0) {
+                    if(time <= this.callbacks[i].time) {
+                        this.callbacks[i].fn();
+                        this.callbacks[i].direction = 1; //set to forwards
                     }
                 }
-            });
+            }
 
-            for (var i = 0; i < this.componentSet.length; i++) {
+            for(var i = 0; i < this.componentSet.length; i++) {
                 var animData = this.componentSet[i];
-
                 for (var j = 0; j < animData.path.length; j++) {
                     var currStep, nextStep;
+
+                    //forward
                     if(this.direction) {
-                        //forward
                         currStep = animData.path[j];
                         nextStep = animData.path[j + 1];
                     } else {
@@ -129,12 +131,8 @@ export class Timeline {
                             for (var k = 0; k < currStep[1].length; k++) {
                                 res[k] = currStep[1][k] + (nextStep[1][k] - currStep[1][k]) * state;
                             }
-
-                            if(animData.component instanceof Size) {
-                                animData.component.setAbsolute.apply(animData.component, res);
-                            } else {
-                                animData.component.set.apply(animData.component, res);
-                            }
+                            if (animData.component instanceof Size) animData.component.setAbsolute.apply(animData.component, res);
+                            else animData.component.set.apply(animData.component, res);
                         } else {
                             animData.component.set(currStep[1] + (nextStep[1] - currStep[1]) * state);
                         }
@@ -142,11 +140,8 @@ export class Timeline {
                     //we are passed last step, set object to final state.
                     if(!nextStep && currStep[0] < time) {
                         if(currStep[1] instanceof Array) {
-                            if(animData.component instanceof Size) {
-                                animData.component.setAbsolute.apply(animData.component, res);
-                            } else {
-                                animData.component.set.apply(animData.component, currStep[1]);
-                            }
+                            if (animData.component instanceof Size) animData.component.setAbsolute.apply(animData.component, res);
+                            else animData.component.set.apply(animData.component, currStep[1]);
                         } else {
                             animData.component.set(currStep[1]);
                         }
